fe889d38e6795ea17bb5506290d0c8e5c5fa0bfc
Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
2015-10-29 11:22:28 -0400

    Migrating module.
     - Updated package.json.
     - Updated Readme.
     - Updated boot.js.
     - Removed bower and travis conf's.
    
    Signed-off-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>

--- test/boot.js
+++ test/boot.js
@@ -7,7 +7,7 @@ if (process.env.RUNNER === 'CI') {
   var junitReporter = new krustyJasmineReporter.KrustyJasmineJUnitReporter({
     specTimer: new jasmine.Timer(),
     JUnitReportSavePath: process.env.SAVE_PATH || './',
-    JUnitReportFilePrefix: process.env.FILE_PREFIX || 'jasmine-n-matchers-results',
+    JUnitReportFilePrefix: process.env.FILE_PREFIX || 'jasmine-n-matchers-results-' +  process.version,
     JUnitReportSuiteName: 'Jasmine N Matchers Reports',
     JUnitReportPackageName: 'Jasmine N Matchers Reports'
   });
